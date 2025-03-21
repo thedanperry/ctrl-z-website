99b52657f45230fc57b40bab7b5d4d9e54d67b39
thedanperry <dperry80@gmail.com>
2025-03-21 14:45:13 -0700

    Update next.config.mjs

--- ctrl-z-website/next.config.mjs
+++ ctrl-z-website/next.config.mjs
@@ -1,21 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-    domains: [
-      "source.unsplash.com",
-      "images.unsplash.com",
-      "ext.same-assets.com",
-      "ugc.same-assets.com",
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
-      },
-    ],
-  },
-};
+  reactStrictMode: true,
+  swcMinify: true,
+}
 
-export default nextConfig;
+module.exports = nextConfig
