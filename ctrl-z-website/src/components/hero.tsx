80d301f3a3f1eb2408a38e18371975accb8eb9e3
thedanperry <dperry80@gmail.com>
2025-04-13 18:38:58 -0700

    Update hero.tsx

--- ctrl-z-website/src/components/hero.tsx
+++ ctrl-z-website/src/components/hero.tsx
@@ -43,7 +43,7 @@ export default function Hero() {
               href="#about"
               className="text-xl uppercase tracking-wide underline-hover"
             >
-              Scroll
+              More↓
             </a>
           </div>
         </div>
